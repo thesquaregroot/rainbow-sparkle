0c5562cbe8200fec4a09d370902b58e007f6350d
Andrew Groot <groot.andrew@gmail.com>
2014-02-03 23:35:56 -0500

    Sparkles are spheres that still just switch between colors (need to improve) but now using lighting and properly reshaping.  Also included some minor interaction with arrow keys.

--- rainbow-sparkle.py
+++ rainbow-sparkle.py
@@ -1,38 +1,69 @@
 #! /usr/bin/env python
 import sys
 from sparkle import *
+from math import sin, cos, radians
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
-screen_width=640
-screen_height=480
+def getSparkles():
+    return [Sparkle()]
 
-sparkles = [Sparkle()]
+class RSGlobals:
+    zoom = 25
+    angle = 0
+    sparkles = getSparkles()
 
 def display():
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    
-    glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    glOrtho(0, screen_width, 0, screen_height, -10, 10)
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity()
+    glClearColor(0, 0, 0, 0)
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
     
-    for sparkle in sparkles:
-        sparkle.render(screen_width/2, screen_height/2, 0)
+    gluLookAt(0, 0, RSGlobals.zoom, 0, 0, 0, 0, 1, 0)
+    glRotatef(-RSGlobals.angle, 0, 1, 0)
+    
+    for sparkle in RSGlobals.sparkles:
+        sparkle.render()
     
     glutSwapBuffers()
 
+def reshape(width, height):
+    glViewport(0, 0, width, height)
+
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(4, float(width)/float(height), 0.2, 1000)
+
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    
+    glEnable(GL_DEPTH_TEST)
+    glLightfv(GL_LIGHT0, GL_POSITION, [1, 1, 1, 0])
+    glLightfv(GL_LIGHT0, GL_DIFFUSE,  [1, 1, 1, 1])
+    glLightfv(GL_LIGHT0, GL_SPECULAR, [1, 1, 1, 1])
+    glEnable(GL_LIGHT0)
+    glEnable(GL_LIGHTING)
+    glShadeModel(GL_SMOOTH)
+
+def keyboard(key, x, y):
+    if (key == GLUT_KEY_UP):
+        RSGlobals.zoom = max(RSGlobals.zoom-1, 1)
+    elif (key == GLUT_KEY_DOWN):
+        RSGlobals.zoom = min(RSGlobals.zoom+1, 50)
+    elif (key == GLUT_KEY_LEFT):
+        RSGlobals.angle += 5
+    elif (key == GLUT_KEY_RIGHT):
+        RSGlobals.angle -= 5
 
 if __name__ == "__main__":
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
-    glutInitWindowSize(screen_width, screen_height)
-    glutInitWindowPosition(0, 0)
+    glutInitWindowSize(640, 640)
     glutCreateWindow(b"Hello, Kate")
-
+    
     glutDisplayFunc(display)
+    glutReshapeFunc(reshape)
+    glutSpecialFunc(keyboard)
     glutIdleFunc(display)
     glutMainLoop()
 
--- sparkle.py
+++ sparkle.py
@@ -1,21 +1,27 @@
 from OpenGL.GL import *
+from OpenGL.GLUT import glutSolidSphere
 
 class Sparkle:
-    WHITE   = [1, 1, 1]
-    RED     = [1, 0, 0]
-    GREEN   = [0, 1, 0]
-    YELLOW  = [1, 1, 0]
-    BLUE    = [0, 0, 1]
+    WHITE   = [1, 1, 1, 1]
+    RED     = [1, 0, 0, 1]
+    GREEN   = [0, 1, 0, 1]
+    YELLOW  = [1, 1, 0, 1]
+    BLUE    = [0, 0, 1, 1]
+    amb_and_diff_prop = [0.7, 0.7, 0.7, 1.0]
+    specular_prop     = [0.8, 0.8, 0.8, 1.0]
 
     def __init__(self):
         self.color = Sparkle.WHITE
     
-    def render(self, i, j, k):
-        glColor3f(self.color[0], self.color[1], self.color[2])
-        glBegin(GL_POINTS)
-        glVertex3f(i, j, k)
-        glEnd()
+    def render(self, i=0, j=0, k=0):
+        glPushMatrix()
+        glTranslatef(i, j, k)
+        glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, [i*j for i,j in zip(self.color,Sparkle.amb_and_diff_prop)])
+        glMaterialfv(GL_FRONT, GL_SPECULAR,            [i*j for i,j in zip(self.color,Sparkle.specular_prop)])
+        glMaterialfv(GL_FRONT, GL_SHININESS,           [80])
+        glutSolidSphere(0.01, 100, 100)
         self.nextColor()
+        glPopMatrix()
     
     def nextColor(self):
         if self.color == Sparkle.WHITE:
@@ -28,5 +34,4 @@ class Sparkle:
             self.color = Sparkle.BLUE
         else:
             self.color = Sparkle.WHITE
-    
 
