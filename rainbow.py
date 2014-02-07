aba4c94cfe8d021aa3c801b6c805860e2db54130
Andrew Groot <groot.andrew@gmail.com>
2014-02-06 23:41:25 -0500

    Rainbow allows for proper adjustment of resolution and has progressive color.

--- rainbow.py
+++ rainbow.py
@@ -5,7 +5,7 @@ class Rainbow:
     DEFAULT_CENTRAL_ANGLE = 180
     DEFAULT_INNER_RADIUS = 0.05
     DEFAULT_OUTER_RADIUS = 0.15
-    RESOLUTION_ANGLE = 5
+    RESOLUTION_ANGLE = 1
     
     RED = [1, 0, 0]
     ORANGE = [1, 0.5, 0]
@@ -21,22 +21,32 @@ class Rainbow:
         self.inner_radius = inner_radius
         self.outer_radius = outer_radius
     
+    def setColor(self, color, next_color, angle, max_angle):
+        red = float((max_angle-angle)*color[0] + angle*next_color[0])/max_angle
+        grn = float((max_angle-angle)*color[1] + angle*next_color[1])/max_angle
+        blu = float((max_angle-angle)*color[2] + angle*next_color[2])/max_angle
+        glColor3f(red, grn, blu)
+    
     def render(self):
         glDisable(GL_LIGHTING)
         glPushMatrix()
-        for color in Rainbow.COLORS:
-            glColor3f(color[0], color[1], color[2])
-            max_angle = self.central_angle / len(Rainbow.COLORS)
-            i = 0
-            while i != max_angle:
+        for color_index in range(len(Rainbow.COLORS)-1):
+            color = Rainbow.COLORS[color_index]
+            next_color = Rainbow.COLORS[color_index+1]
+            max_angle = self.central_angle / (len(Rainbow.COLORS)-1)
+            angle = 0
+            increase = Rainbow.RESOLUTION_ANGLE
+            while angle != max_angle:
+                self.setColor(color, next_color, angle, max_angle)
                 glBegin(GL_QUADS)
                 glVertex3f(self.inner_radius, 0, 0)
                 glVertex3f(self.outer_radius, 0, 0)
                 glVertex3f(self.outer_radius*cos(radians(Rainbow.RESOLUTION_ANGLE)), self.outer_radius*sin(radians(Rainbow.RESOLUTION_ANGLE)), 0)
                 glVertex3f(self.inner_radius*cos(radians(Rainbow.RESOLUTION_ANGLE)), self.inner_radius*sin(radians(Rainbow.RESOLUTION_ANGLE)), 0)
                 glEnd()
-                glRotatef(Rainbow.RESOLUTION_ANGLE, 0, 0, 1)
-                i = min(i+Rainbow.RESOLUTION_ANGLE, max_angle)
+                glRotatef(increase, 0, 0, 1)
+                increase = min(Rainbow.RESOLUTION_ANGLE, max_angle-angle)
+                angle += increase
         glPopMatrix()
         glEnable(GL_LIGHTING)
 
