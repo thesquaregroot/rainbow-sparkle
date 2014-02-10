ac17f3b02372b18c0ca1a44b3b63c897005f1ab1
Andrew Groot <groot.andrew@gmail.com>
2014-02-09 23:16:02 -0500

    Rainbow doesn't look right with lighting--keeping 3D aspect but removing lighting and making the orange normal again.

--- rainbow.py
+++ rainbow.py
@@ -5,10 +5,12 @@ class Rainbow:
     DEFAULT_CENTRAL_ANGLE = 180
     DEFAULT_INNER_RADIUS = 0.05
     DEFAULT_OUTER_RADIUS = 0.15
+    DEFAULT_DEPTH = 0.01
+    DEFAULT_OPACITY = 1
     RESOLUTION_ANGLE = 1
     
     RED = [1, 0, 0]
-    ORANGE = [1, 0.3, 0]
+    ORANGE = [1, 0.5, 0]
     YELLOW = [1, 1, 0]
     GREEN = [0, 1, 0]
     BLUE = [0, 0, 1]
@@ -16,17 +18,22 @@ class Rainbow:
 
     COLORS = [RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE]
 
-    def __init__(self, central_angle = DEFAULT_CENTRAL_ANGLE, inner_radius = DEFAULT_INNER_RADIUS, outer_radius = DEFAULT_OUTER_RADIUS):
+    def __init__(self, central_angle = DEFAULT_CENTRAL_ANGLE, inner_radius = DEFAULT_INNER_RADIUS, outer_radius = DEFAULT_OUTER_RADIUS, depth=DEFAULT_DEPTH, opacity=DEFAULT_OPACITY):
         self.central_angle = central_angle
         self.inner_radius = inner_radius
         self.outer_radius = outer_radius
+        self.depth = depth
+        self.opacity = opacity
     
     def setColor(self, color, next_color, angle, max_angle):
-        red = float((max_angle-angle)*color[0] + angle*next_color[0])/max_angle
+        red = float((max_angle-angle)*color[0] + (angle+1)*next_color[0])/max_angle
         grn = float((max_angle-angle)*color[1] + angle*next_color[1])/max_angle
         blu = float((max_angle-angle)*color[2] + angle*next_color[2])/max_angle
         glColor3f(red, grn, blu)
-    
+#        glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, [red, grn, blu, self.opacity])
+#        glMaterialfv(GL_FRONT, GL_SPECULAR, [red, grn, blu, self.opacity])
+#        glMaterialfv(GL_FRONT, GL_SHININESS, [10])
+
     def render(self):
         glDisable(GL_LIGHTING)
         glPushMatrix()
@@ -35,17 +42,35 @@ class Rainbow:
             next_color = Rainbow.COLORS[color_index+1]
             max_angle = self.central_angle / (len(Rainbow.COLORS)-1)
             angle = 0
-            increase = Rainbow.RESOLUTION_ANGLE
             while angle != max_angle:
+                increase = min(Rainbow.RESOLUTION_ANGLE, max_angle-angle)
                 self.setColor(color, next_color, angle, max_angle)
+                glBegin(GL_QUAD_STRIP)
+                # create square cylinder (slight fan)
+                glVertex3f(self.inner_radius, 0, self.depth)
+                glVertex3f(self.outer_radius, 0, self.depth)
+                glVertex3f(self.inner_radius*cos(radians(increase)), self.inner_radius*sin(radians(increase)), self.depth)
+                glVertex3f(self.outer_radius*cos(radians(increase)), self.outer_radius*sin(radians(increase)), self.depth)
+                glVertex3f(self.inner_radius*cos(radians(increase)), self.inner_radius*sin(radians(increase)), -self.depth)
+                glVertex3f(self.outer_radius*cos(radians(increase)), self.outer_radius*sin(radians(increase)), -self.depth)
+                glVertex3f(self.inner_radius, 0, -self.depth)
+                glVertex3f(self.outer_radius, 0, -self.depth)
+                glVertex3f(self.inner_radius, 0, self.depth)
+                glVertex3f(self.outer_radius, 0, self.depth)
+                glEnd()
                 glBegin(GL_QUADS)
-                glVertex3f(self.inner_radius, 0, 0)
-                glVertex3f(self.outer_radius, 0, 0)
-                glVertex3f(self.outer_radius*cos(radians(Rainbow.RESOLUTION_ANGLE)), self.outer_radius*sin(radians(Rainbow.RESOLUTION_ANGLE)), 0)
-                glVertex3f(self.inner_radius*cos(radians(Rainbow.RESOLUTION_ANGLE)), self.inner_radius*sin(radians(Rainbow.RESOLUTION_ANGLE)), 0)
+                # close top with square
+                glVertex3f(self.outer_radius, 0, self.depth)
+                glVertex3f(self.outer_radius*cos(radians(increase)), self.outer_radius*sin(radians(increase)), self.depth)
+                glVertex3f(self.outer_radius*cos(radians(increase)), self.outer_radius*sin(radians(increase)), -self.depth)
+                glVertex3f(self.outer_radius, 0, -self.depth)
+                # close bottom with square
+                glVertex3f(self.inner_radius, 0, self.depth)
+                glVertex3f(self.inner_radius*cos(radians(increase)), self.inner_radius*sin(radians(increase)), self.depth)
+                glVertex3f(self.inner_radius*cos(radians(increase)), self.inner_radius*sin(radians(increase)), -self.depth)
+                glVertex3f(self.inner_radius, 0, -self.depth)
                 glEnd()
                 glRotatef(increase, 0, 0, 1)
-                increase = min(Rainbow.RESOLUTION_ANGLE, max_angle-angle)
                 angle += increase
         glPopMatrix()
         glEnable(GL_LIGHTING)
