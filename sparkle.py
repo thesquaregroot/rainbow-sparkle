7d96d2e90b17c4c24019b4ca98ec7a86e0fad4d0
Andrew Groot <groot.andrew@gmail.com>
2014-02-09 23:16:43 -0500

    Modeling sparkles as a single rotating triangle.  Fairly zoomed out it seems normal.

--- sparkle.py
+++ sparkle.py
@@ -5,7 +5,7 @@ class Sparkle:
     GOLD    = [1, 0.5, 0, 1]
     dull_amb_and_diff    = [0.1, 0.1, 0.1, 1.0]
     dull_specular        = [0.2, 0.2, 0.2, 1.0]
-    bright_amb_and_diff  = [0.6, 0.6, 0.6, 1.0]
+    bright_amb_and_diff  = [0.8, 0.8, 0.8, 1.0]
     bright_specular      = [0.8, 0.8, 0.8, 1.0]
     
     def __init__(self):
@@ -15,26 +15,16 @@ class Sparkle:
     def render(self, i=0, j=0, k=0):
         glPushMatrix()
         glTranslatef(i, j, k)
+        glScalef(0.01, 0.01, 0.01)
+        glRotatef(5*self.frame, 0, 1, 0)
+        glRotatef(10*self.frame, 1, 0, 0)
         glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, [i*j for i,j in zip(self.color, Sparkle.bright_amb_and_diff)])
         glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR,            [i*j for i,j in zip(self.color, Sparkle.bright_specular)])
         glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, [80])
-        glScalef(0.004, 0.004, 0.004)
-        glRotatef(15*self.frame, 1, 0, 0)
-        glRotatef(10*self.frame, 0, 1, 0)
-        glRotatef( 5*self.frame, 0, 0, 1)
         glBegin(GL_TRIANGLES)
-        #tringle1
-        glVertex3f( 1,  1,  0)
-        glVertex3f( 1,  0,  1)
-        glVertex3f( 0,  1,  1)
-        #tringle2
-        glVertex3f( 0,  0,  1)
-        glVertex3f( 1,  0,  0)
-        glVertex3f( 0,  1,  0)
-        #tringle2
-        glVertex3f( 0,  1,  1)
-        glVertex3f( 1,  0,  0)
-        glVertex3f( 0,  1,  0)
+        glVertex3f(   0,   0.3,  0)
+        glVertex3f(-0.2, -0.2,  0)
+        glVertex3f( 0.2, -0.2,  0)
         glEnd()
         glPopMatrix()
         self.frame += 1
