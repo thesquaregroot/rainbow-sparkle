2f57c8e86d1d54ffb231234055481a612a123819
Joshua Green <josh@softwareverde.com>
2014-02-19 02:20:22 -0500

    Gave sparkles' constructor position variables. Made sparkles start in a random sparkle state.

--- sparkle.py
+++ sparkle.py
@@ -1,5 +1,6 @@
 from OpenGL.GL import *
 from OpenGL.GLUT import *
+import random
 
 class Sparkle:
     GOLD    = [1, 0.5, 0, 1]
@@ -8,14 +9,17 @@ class Sparkle:
     bright_amb_and_diff  = [0.8, 0.8, 0.8, 1.0]
     bright_specular      = [0.8, 0.8, 0.8, 1.0]
     
-    def __init__(self):
+    def __init__(self, i=0, j=0, k=0):
         self.color = Sparkle.GOLD
-        self.frame = 0
+        self.frame = random.randint(0, 1024)
+        self.i = i
+        self.j = j
+        self.k = k
     
     def render(self, i=0, j=0, k=0):
         glPushMatrix()
-        glTranslatef(i, j, k)
         glScalef(0.01, 0.01, 0.01)
+        glTranslatef(i + self.i, j + self.j, k + self.k)
         glRotatef(5*self.frame, 0, 1, 0)
         glRotatef(10*self.frame, 1, 0, 0)
         glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, [i*j for i,j in zip(self.color, Sparkle.bright_amb_and_diff)])
